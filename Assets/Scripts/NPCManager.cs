f1ac90a489db00ec717f4494962e65f688efbebc
Fuhans Puji Saputra <fuhansxavega92@gmail.com>
2015-02-09 00:58:54 +0800

    Patrol NPC with Interaction

---
+++ Assets/Scripts/NPCManager.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPCManager : MonoBehaviour 
+{
+    private PlayerManager _playerManager; // Referensi PlayerManager Script
+
+    private bool moveForward = false; // Bwt NPC maju kedepan
+
+    private float movementSpeed = 1.5f; // Bwt kecepatan NPC maju
+
+    private void Awake()
+    {
+        // Cari gameobject yg namanya Player dan ambil PlayerManager script yg ad di gameobject Player
+        _playerManager = GameObject.Find("Player").GetComponent<PlayerManager>();
+    }
+
+    private void Update()
+    {
+        // Kalo NPC maju kedepan
+        if (moveForward)
+        {
+            // Suruh NPC maju kedepan dikaliin dgn kecepatan NPC maju dikaliin per frame
+            transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+        }
+
+        // Kalo NPC ga maju kedepan
+        else
+        {
+            // Panggil Hold method
+            StartCoroutine(Hold());
+        }
+    }
+
+    private void OnGUI()
+    {
+        // Kalo karakter lg bicara
+        if (_playerManager.isOnChat)
+        {
+            // Tampilin persegi dgn posisi lebar ambil dgn lebar screen resolution di unity dibagi dgn 2 dan posisi tinggi dgn tinggi screen resolution di unity dibagi dgn 2
+            // Tampilin persegi dgn lebar 250 pixels dan tinggi 100 pixels dan tampilin pesan sperti yg ad dibawah
+            GUI.Box(new Rect(Screen.width / 2, Screen.height / 2, 250, 100), "Kamu mau bicara sama saya? \n\nApa yang mau kamu bicarakan? \n\nTapi saya sedang sibuk.");
+        }
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        // Kalo NPC nabrak game object yg namanya First Point
+        if (col.gameObject.name == "First Point")
+        {
+            // Bwt karakter maju true
+            moveForward = true;
+        }
+
+        // Kalo NPC nabrak game object yg namanya Second Point
+        else if (col.gameObject.name == "Second Point")
+        {
+            // Bwt karakter maju false
+            moveForward = false;
+        }
+    }
+
+    private IEnumerator Hold()
+    {
+        // Tunggu 2 detik baru execute kode yg dibawah
+        yield return new WaitForSeconds(2.0f);
+
+        // Sruh NPC maju kebelakang dikaliin kecepeatan NPC maju dikaliin per frame
+        transform.Translate(-Vector3.forward * movementSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(CharacterController))] // Butuh CharacterController di game object yg lu mw mskin script ini
+public class PlayerManager : MonoBehaviour 
+{
+    private CharacterController controller; // Referensi CharacterController, namain controller
+
+    private Vector3 moveDirection = Vector3.zero; // Bwt arah kmana karakter jalan
+
+    private float gravity = 20.0f, speed = 5.0f; // Bwt daya berat karakter sm cepet jalan nya
+
+    public bool isOnChat = false; // Bwt tw kalo player lg bicara
+
+    private void Awake()
+    {
+        // Ambil referensi CharacterController dr game object yg lu mskin script ini
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        // Kalo karakter ada di lantai
+        if (controller.isGrounded)
+        {
+            // Render karakter spy bs diliat oleh kt
+            gameObject.renderer.enabled = true;
+
+            // Horizontal n Vertical Axis (Unity Default Axis), Horizontal (A, D), Vertical (W, S)
+            moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+            // Bwt arah karakter sm dgn arah karakter based on ap yg lu pncet di keyboard (W, A, S ato D)
+            moveDirection = transform.TransformDirection(moveDirection);
+
+            // Kali in arah karakter jalan dgn kecepatan jalan
+            moveDirection *= speed;
+        }
+
+        // Kalo karakter it ada di langit, bwt karakter turun dan dikurang daya berat dikali dgn per frame
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Bwt karakter denger ap yg lu pncet di keyboard (W, A, S ato D) based on arah karakter dikali dgn per frame
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        // Kalo karakter nabrak gameobjet yg namanya NPC Follower
+        if (col.gameObject.name == "NPC Follower")
+        {
+            // Bwt karakter lg bicara
+            isOnChat = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider col)
+    {
+        // Kalo karakter lg ga nabrak gameobject yg namanya NPC Follower
+        if (col.gameObject.name == "NPC Follower")
+        {
+            // Bwt karakter ga bicara lg
+            isOnChat = false;
+        }
+    }
+}
